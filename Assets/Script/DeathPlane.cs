61857cf784c1bedd7294f81b0d0e9ef933755017
JacobLau1 <93946921+JacobLau1@users.noreply.github.com>
2023-09-30 12:14:14 -0400

    fixed camera, added endmenu, made basic level2, made gamemanager basic, trying death plane and trap scripts

--- Assets/Script/DeathPlane.cs
+++ Assets/Script/DeathPlane.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class DeathPlane : MonoBehaviour
 {
+    public GameObject Player;
+    Vector3 OriginalPlayerPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OriginalPlayerPosition = Player.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            RestartGame();
+        }
+    }
+
+    public void RestartGame()
+    {
+        Player.transform.position = OriginalPlayerPosition;
     }
 }
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public float Score = 0;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+
+    public void IncrementScore()
+    {
+        // TODO Increment score logic and win condition 
+        ++Score;
+        if (Score >= 4)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //load next scene
+        }
+
+    }
+}
