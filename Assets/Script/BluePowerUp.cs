352d86491fe4810d6caca32ee722ee07745d5a36
JacobLau1 <93946921+JacobLau1@users.noreply.github.com>
2023-09-30 20:47:25 -0400

    added spin and floating to powerups

--- Assets/Script/BluePowerUp.cs
+++ Assets/Script/BluePowerUp.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BluePowerUp : MonoBehaviour
 {
+
+    public ParticleSystem particleSystemPrefab;
+
+    public float floatSpeed = 1.0f;  // Speed of floating.
+    public float rotationSpeed = 30.0f;  // Speed of spinning.
+    public float floatAmplitude = 0.5f; // Amplitude of floating motion.
+
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Store the initial position to use as the reference point for floating.
+        initialPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Float the object up and down.
+        Vector3 newPosition = initialPosition;
+        newPosition.y += Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
+        transform.position = newPosition;
+
+        // Spin the object.
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
+
+  
 }
--- Assets/Script/YellowPowerUp.cs
+++ Assets/Script/YellowPowerUp.cs
@@ -6,6 +6,30 @@ using UnityEngine.SceneManagement;
 public class YellowPowerUp : MonoBehaviour
 {
     public ParticleSystem particleSystemPrefab;
+
+    public float floatSpeed = 1.0f;  // Speed of floating.
+    public float rotationSpeed = 30.0f;  // Speed of spinning.
+    public float floatAmplitude = 0.5f; // Amplitude of floating motion.
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        // Store the initial position to use as the reference point for floating.
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Float the object up and down.
+        Vector3 newPosition = initialPosition;
+        newPosition.y += Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
+        transform.position = newPosition;
+
+        // Spin the object.
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == ("Player"))
@@ -14,10 +38,6 @@ public class YellowPowerUp : MonoBehaviour
             Debug.Log("Player touched the yellowpowerup!");
 
             Vector3 boxPosition = transform.position;
-
-            Debug.Log(boxPosition);
-           
-            //Destroy(gameObject);
 
             // Create and position the Particle System at the box's location.
             ParticleSystem particles = Instantiate(particleSystemPrefab, boxPosition, Quaternion.identity);
